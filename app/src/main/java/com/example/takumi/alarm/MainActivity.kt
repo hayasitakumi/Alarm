2c05cfd4a27a50df95a675929fb5f0334136674e
hayasitakumi <b1701571@planet.kanazawa-it.ac.jp>
2018-12-18 03:34:22 +0900

    create sound feature and timer event

--- app/src/main/java/com/example/takumi/alarm/MainActivity.kt
+++ app/src/main/java/com/example/takumi/alarm/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.takumi.alarm
 
-import android.media.MediaPlayer
+import android.content.Context
 import android.os.Bundle
+import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.*
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import me.mattak.moment.Moment
 
 open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +18,6 @@ open class MainActivity : AppCompatActivity() {
 
         val spinner_H = findViewById<View>(R.id.spinner_Hour) as Spinner
         val spinner_M = findViewById<View>(R.id.spinner_Minute) as Spinner
-        val spinner_S = findViewById<View>(R.id.spinner_Second) as Spinner
 
         val adapter_H = ArrayAdapter.createFromResource(
             this,
@@ -28,52 +27,27 @@ open class MainActivity : AppCompatActivity() {
             this,
             R.array.str_Minute, android.R.layout.simple_spinner_item
         )
-        val adapter_S = ArrayAdapter.createFromResource(
-            this,
-            R.array.str_Minute, android.R.layout.simple_spinner_item
-        )
 
         adapter_H.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         adapter_M.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        adapter_S.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinner_H.adapter = adapter_H
-        Log.d("spinner", "debug")
         spinner_M.adapter = adapter_M
-        spinner_S.adapter = adapter_S
 
-//        val textView: TextView = findViewById(R.id.timer) as TextView
-//
-//        val calendar = Calendar.getInstance()
-//        val hour = calendar.get(Calendar.HOUR)
-//        val minute = calendar.get(Calendar.MINUTE)
-//        val second = calendar.get(Calendar.SECOND)
-//        textView.text = "${hour}時${minute}分${second}秒"
-        val moment = Moment() // 引数に何も指定しなければ現在の日付が入る
-        Log.d("debug", "${moment}")
+        val timer = TimerTask()
+        timer.load(spinner_H, spinner_M)
 
-        val sound_but: Button = findViewById(R.id.sound_button)
         val sound = Music()
         sound.load(assets.openFd("Japanese_School_Bell04-1.mp3"))
-//
-//
-//        val mediaPlayer = MediaPlayer()
-//        val afdescripter = assets.openFd("Japanese_School_Bell04-1.mp3")
-//        mediaPlayer.setDataSource(
-//            afdescripter.getFileDescriptor(),
-//            afdescripter.getStartOffset(),
-//            afdescripter.getLength()
-//        )
-//        mediaPlayer.prepare()
-//        mediaPlayer.start()
+        val vibrator: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-        sound_but.setOnClickListener{
-            sound.start()
+        timer.run(sound,vibrator)
+
+        val stop_but:Button = findViewById(R.id.button)
+        stop_but.setOnClickListener{
+            sound.stop()
         }
 
-//        button.setOnClickListener{
-//            val vibrator: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-//            vibrator.vibrate(300)
     }
 }
 
--- app/src/main/java/com/example/takumi/alarm/Music.kt
+++ app/src/main/java/com/example/takumi/alarm/Music.kt
@@ -2,23 +2,26 @@ package com.example.takumi.alarm
 
 import android.content.res.AssetFileDescriptor
 import android.media.MediaPlayer
-import android.support.v7.app.AppCompatActivity
 
-open class Music : AppCompatActivity() {
+open class Music{
     val mediaPlayer = MediaPlayer()
     lateinit var afdescripter: AssetFileDescriptor
 
     open fun load(fd: AssetFileDescriptor) {
         afdescripter = fd
-    }
-
-    open fun start() {
         mediaPlayer.setDataSource(
             afdescripter.getFileDescriptor(),
             afdescripter.getStartOffset(),
             afdescripter.getLength()
         )
         mediaPlayer.prepare()
+    }
+
+    open fun start() {
         mediaPlayer.start()
+    }
+
+    fun stop(){
+        mediaPlayer.stop()
     }
 }
